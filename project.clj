2b2324f1b3b74f54078eb6f292ce8b71921d6888
faineBenk <shdvvch1ld@proton.me>
2024-07-25 14:15:54 +0530

    init commit

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject snake "0.1.0-SNAPSHOT"
+  :description "snake with parentheses"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [org.clojure/tools.logging "1.2.3"]]
+  :resource-paths ["lib"]
+  :main ^:skip-aot snake.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
